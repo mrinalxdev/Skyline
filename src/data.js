2acdd6345e2e0053e2bf32cb66f536b2b639fd14
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-09-06 14:56:18 +0530

    Added Web Data

---
+++ src/data.js
@@ -0,0 +1,104 @@
+import shippingIcon from "./assets/free-shiping-icon.svg";
+import paymentIcon from "./assets/quick-payment-icon.svg";
+import supportIcon from "./assets/support-icon.svg";
+import productOneIcon from "./assets/product-one.svg";
+import productTwoIcon from "./assets/produc-two.svg";
+import productThreeIcon from "./assets/product-three.svg";
+import plantIcon from "./assets/plant-icon.svg";
+import sunIcon from "./assets/sun-icon.svg";
+import waterIcon from "./assets/water-icon.svg";
+import temperatureIcon from "./assets/temperature-icon.svg";
+
+export const heroTitle = "Bring Serenity to Your Place with Interior Plants";
+
+export const heroSubtitle =
+  "find your dream plant for your home decoration with us, and we will make it happen";
+
+export const services = [
+  {
+    title: "Free Shipping",
+    subtitle: "No charge for your delivery",
+    icon: shippingIcon,
+  },
+  {
+    title: "Quick Payment",
+    subtitle: "100% secure",
+    icon: paymentIcon,
+  },
+  {
+    title: "24/7",
+    subtitle: "Don't hesitate to contact us",
+    icon: supportIcon,
+  },
+];
+
+export const productsTitle = "Best Seeling Products";
+export const productBtnText = "See Collection";
+
+export const products = [
+  {
+    title: "Golden Pothos",
+    price: "Rs. 160",
+    img: productOneIcon,
+  },
+  {
+    title: "Rubber Plant",
+    price: "Rs. 210",
+    img: productTwoIcon,
+  },
+  {
+    title: "Cast Iron Plant",
+    price: "Rs. 310",
+    img: productThreeIcon,
+  },
+];
+
+export const referenceTitle = "Interior Plants Reference";
+export const referenceSubtitle = "make your home cozy with refreshing plants";
+
+export const careTitle = "How to care for plants";
+export const careSubtitle = "Take care of plants with all your heart";
+
+export const careList = [
+  {
+    title: "Adjust Lighting",
+    subtitle:
+      "When caring for houseprints, make sure the room temprature is neither too cold nor too hot",
+    img: sunIcon,
+  },
+  {
+    title: "Choose the right container",
+    subtitle:
+      "Houseplants will need different containers depending on their type and size.",
+    img: plantIcon,
+  },
+  {
+    title: "Fertilize regurlarly",
+    subtitle:
+      "The nutrients most indoor houseplants need are nitrogen for balance and potassium for stem strength,",
+    img: temperatureIcon,
+  },
+];
+
+export const newsletter = "Newsletter";
+export const footerLinksColumnOne = [
+  {
+    title: "Support",
+    links: ["About Us", "Careers", "Blog"],
+  },
+  {
+    title: "Useful Links",
+    links: ["Payment & Tax", "Terms of service", "Privacy Policy"],
+  },
+];
+
+export const footerLinksColumnTwo = [
+  {
+    title: "Our Menu",
+    links: ["Best Products", "Categories"],
+  },
+  {
+    title: "Address",
+    links: ["Delhi Karol Bagh main shop, Shop No. 56", "Hemant Mishra"],
+  },
+];
